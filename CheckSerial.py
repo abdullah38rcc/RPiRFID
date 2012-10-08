daf62b371e065c93c5e4d907d825c874157d263d
Amos K <amos.kittelson@gmail.com>
2012-10-08 05:11:08 +0000

    Added Twisted HTTP Server

--- CheckSerial.py
+++ CheckSerial.py
@@ -1,5 +1,33 @@
-
+from twisted.web.server import Site
+from twisted.web.resource import Resource
+from twisted.internet import reactor
+import RPi.GPIO as GPIO
 import urllib
+import threading
+import time
+
+# Web content handler
+class ReturnWebpage(Resource):
+    def __init__(self, serialNum):
+        Resource.__init__(self)
+        self.serialNum = serialNum
+
+    def render_GET(self, request):
+        return "<html><body><pre>%s</pre></body></html>" % (self.serialNum,)
+
+# Root webpage
+class getSerial(Resource):
+  def getChild(self, name, request):
+      return ReturnWebpage(int(name))
+
+class FlashLED(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+	def run(self):
+		print 'flashing led'
+		GPIO.setup(12, GPIO.HIGH)
+		time.sleep(1)
+		GPIO.setup(12, GPIO.LOW)
 
 def download(url):
     """ Download file from web and save """
@@ -18,9 +46,22 @@ def findSerial(fileName, textString):
 
     search = textString
     index = text.find(search)
-    print search, "found at index ", index
+    if index > 0:
+        print search, "found at index ", index
+        background = FlashLED()
+        background.start()
+    else:
+        print 'Serial not found'
 
 
+def GPIOinit():
+    # Set GPIO for RPi pin numbers
+    GPIO.setmode(GPIO.BOARD)
+    # set 12 as output
+    GPIO.setup(12, GPIO.OUT)
+    # set 12 low
+    GPIO.setup(12, GPIO.LOW)
+	
 if __name__ == '__main__':
     import sys
     if len(sys.argv) == 2:
@@ -29,6 +70,15 @@ if __name__ == '__main__':
         except IOError:
             print 'Filename not found.'
     else:
+        # Start web server
+        root = getSerial()
+        factory = Site(root)
+        reactor.listenTCP(8880, factory)
+        reactor.run()
+
+        # Setup GPIO
+        GPIOinit()
+
         download('http://dl.dropbox.com/u/2435953/dlserials.txt')
         findSerial('dlserials.txt', "353568")
 
